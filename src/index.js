067f40d251550ac11bab153bde8617ababf46572
William Kasel <wkasel@gmail.com>
2018-12-13 11:54:22 -0800

    install react

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+class ReactSimpleQuestionnaire extends React.PureComponent {
+  render() {
+    return <div>This is so Fancy!</div>;
+  }
+}
+export default ReactSimpleQuestionnaire;
