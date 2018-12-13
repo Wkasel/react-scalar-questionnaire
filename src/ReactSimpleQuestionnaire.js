63af55a3357cede11979a580ede99ce6197abce4
William Kasel <wkasel@gmail.com>
2018-12-13 12:58:03 -0800

    config webpack

---
+++ src/ReactSimpleQuestionnaire.js
@@ -0,0 +1,7 @@
+import React from "react";
+class ReactSimpleQuestionnaire extends React.PureComponent {
+  render() {
+    return <div>React Simple Questionnaire is ready to roll...</div>;
+  }
+}
+export default ReactSimpleQuestionnaire;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,1 @@
-import React from "react";
-class ReactSimpleQuestionnaire extends React.PureComponent {
-  render() {
-    return <div>This is so Fancy!</div>;
-  }
-}
-export default ReactSimpleQuestionnaire;
+export { default } from "./ReactSimpleQuestionnaire";
